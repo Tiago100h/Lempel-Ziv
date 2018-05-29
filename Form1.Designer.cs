93356d3d69196cb7f5242b1c76dba3aaff8197c4
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2018-05-29 19:02:12 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Lempel_Ziv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Compactar = new System.Windows.Forms.Button();
+            this.Descompactar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Compactar
+            // 
+            this.Compactar.Location = new System.Drawing.Point(13, 13);
+            this.Compactar.Name = "Compactar";
+            this.Compactar.Size = new System.Drawing.Size(125, 23);
+            this.Compactar.TabIndex = 0;
+            this.Compactar.Text = "Compactar...";
+            this.Compactar.UseVisualStyleBackColor = true;
+            this.Compactar.Click += new System.EventHandler(this.Compactar_Click);
+            // 
+            // Descompactar
+            // 
+            this.Descompactar.Location = new System.Drawing.Point(13, 78);
+            this.Descompactar.Name = "Descompactar";
+            this.Descompactar.Size = new System.Drawing.Size(125, 23);
+            this.Descompactar.TabIndex = 1;
+            this.Descompactar.Text = "Descompactar...";
+            this.Descompactar.UseVisualStyleBackColor = true;
+            this.Descompactar.Click += new System.EventHandler(this.Descompactar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(150, 113);
+            this.Controls.Add(this.Descompactar);
+            this.Controls.Add(this.Compactar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Compactar;
+        private System.Windows.Forms.Button Descompactar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lempel_Ziv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int Contador = 0;
+        public string ContadorBinario
+        {
+            get
+            {
+                return Convert.ToString(Contador, 2);
+            }
+        }        
+
+        private void Compactar_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog
+            {
+                Filter = "TXT files|*.txt"
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // Lendo arquivo
+                string arquivo;
+                using (var sr = new System.IO.StreamReader(ofd.FileName))
+                {
+                    arquivo = sr.ReadToEnd();
+                }
+
+                // Variáveis para controlar o loop e o dicionário
+                Contador = 0;
+                var indice = 0;
+                var tamanho = 1;
+                var sequenciaNova = string.Empty;
+                Dicionario ultimaSequenciaEncontrada = null;
+
+                // Criando o dicionário e adicionando o simbolo vazio
+                var dicionario = new List<Dicionario>
+                {
+                    new Dicionario { PalavraCodigo = ContadorBinario, Sequencia = "" }
+                };
+
+                // Criando variável para armazenar o conteúdo com bits e bytes
+                var sequenciaCodificada = new List<SequenciaCodificada>();
+
+                // Percorrendo arquivo
+                while (indice < arquivo.Length && indice + tamanho < arquivo.Length + 1)
+                {
+                    sequenciaNova = arquivo.Substring(indice, tamanho);
+                    var sequenciaDicionario = dicionario.Find(d => d.Sequencia == sequenciaNova);
+
+                    if (sequenciaDicionario is null)
+                    {
+                        if (ultimaSequenciaEncontrada is null)
+                        {
+                            for (int i = 0; i < dicionario.Last().PalavraCodigo.Length; i++)
+                            {
+                                sequenciaCodificada.Add(new SequenciaCodificada { Sequencia = dicionario.First().PalavraCodigo, Bit = true });
+                            }
+                            sequenciaCodificada.Add(new SequenciaCodificada { Sequencia = sequenciaNova, Bit = false });
+                            Contador++;
+                            dicionario.Add(new Dicionario { PalavraCodigo = ContadorBinario, Sequencia = sequenciaNova });
+                            indice += tamanho;
+                            tamanho = 1;
+                        }
+                        else
+                        {
+                            for (int i = ultimaSequenciaEncontrada.PalavraCodigo.Length; i < dicionario.Last().PalavraCodigo.Length; i++)
+                            {
+                                sequenciaCodificada.Add(new SequenciaCodificada { Sequencia = dicionario.First().PalavraCodigo, Bit = true });
+                            }
+                            sequenciaCodificada.Add(new SequenciaCodificada { Sequencia = ultimaSequenciaEncontrada.PalavraCodigo, Bit = true });
+                            sequenciaCodificada.Add(new SequenciaCodificada { Sequencia = sequenciaNova.Last().ToString(), Bit = false });
+                            Contador++;
+                            dicionario.Add(new Dicionario { PalavraCodigo = ContadorBinario, Sequencia = sequenciaNova });
+                            indice += tamanho;
+                            tamanho = 1;
+                        }
+                        ultimaSequenciaEncontrada = null;
+                        sequenciaNova = string.Empty;
+                    }
+                    else
+                    {
+                        ultimaSequenciaEncontrada = sequenciaDicionario;
+                        tamanho++;
+                    }
+                }
+
+                // Última sequência
+                if (!string.IsNullOrWhiteSpace(sequenciaNova))
+                {
+                    if (ultimaSequenciaEncontrada is null)
+                    {
+                        for (int i = 0; i < dicionario.Last().PalavraCodigo.Length; i++)
+                        {
+                            sequenciaCodificada.Add(new SequenciaCodificada { Sequencia = dicionario.First().PalavraCodigo, Bit = true });
+                        }
+                        sequenciaCodificada.Add(new SequenciaCodificada { Sequencia = sequenciaNova, Bit = false });
+                    }
+                    else
+                    {
+                        for (int i = ultimaSequenciaEncontrada.PalavraCodigo.Length; i < dicionario.Last().PalavraCodigo.Length; i++)
+                        {
+                            sequenciaCodificada.Add(new SequenciaCodificada { Sequencia = dicionario.First().PalavraCodigo, Bit = true });
+                        }
+                        sequenciaCodificada.Add(new SequenciaCodificada { Sequencia = ultimaSequenciaEncontrada.PalavraCodigo, Bit = true });
+                    }
+                }
+
+                // Removendo 0 inicial
+                sequenciaCodificada.RemoveAt(0);
+
+                // Substituindo bytes por bits
+                var zerosEum = string.Join(string.Empty, sequenciaCodificada.Select(s => s.Bit ? s.Sequencia : Convert.ToString(s.Sequencia.ToCharArray()[0], 2).PadLeft(8, '0')));
+
+                // Convertendo bits em bytes
+                var bytes = new List<byte>();
+                int j = 0;
+                while (j + 8 <= zerosEum.Length)
+                {
+                    var oitoBits = zerosEum.Substring(j, 8);
+                    var bitsEmByte = Convert.ToByte(oitoBits, 2);
+                    bytes.Add(bitsEmByte);
+                    j += 8;
+                }
+
+                // Último byte
+                if(j + 8 == zerosEum.Length)
+                {
+                    bytes.Add(Convert.ToByte(0));
+                }
+                else
+                {
+                    var bitsRestantes = zerosEum.Length - j;
+                    bytes.Add(Convert.ToByte(zerosEum.Substring(j, bitsRestantes).PadLeft(8, '0'), 2));
+                    bytes.Add(Convert.ToByte(8 - bitsRestantes));
+                }
+
+                // Gerando arquivo
+                var arquivoRetorno = ofd.FileName + ".lz";
+                System.IO.File.WriteAllBytes(arquivoRetorno, bytes.ToArray());
+
+                MessageBox.Show("Arquivo gerado:" + Environment.NewLine + arquivoRetorno);
+            }
+        }
+
+        private void Descompactar_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // Lendo arquivo
+                var arquivo = System.IO.File.ReadAllBytes(ofd.FileName);
+
+                // Verificando quantidade de zeros adicionados na conversão
+                var zerosAdicionados = Convert.ToInt32(arquivo.Last());
+
+                // Removendo último byte
+                var bytes = arquivo.ToList();
+                bytes.Remove(bytes.Last());
+
+                // Convertendo bytes em bits
+
+            }
+        }
+    }
+
+    public class Dicionario
+    {
+        public string PalavraCodigo;
+        public string Sequencia;
+    }
+
+    public class SequenciaCodificada
+    {
+        public bool Bit;
+        public string Sequencia;
+    }
+}
